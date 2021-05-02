663251f269522c77d86a880b4c60505cefb3f5d9
szangyi <alexandra.mszrs@gmail.com>
2021-05-02 21:33:59 +0200

    nav is ready

---
+++ my_jquery.js
@@ -0,0 +1,41 @@
+// --masthead--
+
+jQuery(document).ready(function ($) {
+  var $masthead = $(".masthead");
+
+  var mastheadWidth = 0;
+  function getMastheadWidth() {
+    mastheadWidth = $masthead.get(0).scrollWidth;
+  }
+  getMastheadWidth();
+  ScrollTrigger.addEventListener("refreshInit", getMastheadWidth);
+
+  function updateProxy() {
+    // move the handler to the corresponding ratio according to the page's scroll position.
+    if (mastheadScrollTrigger) {
+      gsap.set(proxy, { x: -mastheadScrollTrigger.scroll(), overwrite: "auto" });
+    }
+  }
+
+  var mastheadScrollTrigger = ScrollTrigger.create({
+    id: "masthead",
+    animation: gsap.to(".masthead", {
+      x: function () {
+        return -(mastheadWidth - window.innerWidth);
+      },
+      ease: "none",
+    }),
+    trigger: ".masthead",
+    end: function () {
+      return mastheadWidth;
+    },
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    invalidateOnRefresh: true,
+  });
+
+  var proxy = document.createElement("div");
+
+  window.addEventListener("wheel", updateProxy);
+});
