022593338e76061e4be24c8d6bb8da72b0d00829
szangyi <alexandra.mszrs@gmail.com>
2021-05-09 20:03:29 +0200

    parallex added and works

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import "/style.scss";
 
 // *JS functions
+
+// *parallex on images
+// https://simpleparallax.com
+
+var images = document.querySelectorAll("img");
+new simpleParallax(images);
+
+new simpleParallax(images, {
+  scale: 1,
+});
