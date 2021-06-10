fdda893139bd386b86721269c97526e3c5c987a2
Alexandra Mészáros <alexandrameszaros@Alex-Macbook.local>
2021-06-10 17:42:20 +0200

    works on safari

--- myjquery.js
+++ myjquery.js
@@ -39,3 +39,32 @@ jQuery(document).ready(function ($) {
 
   window.addEventListener("wheel", updateProxy);
 });
+
+//sections appearing
+// const sections = [...document.querySelectorAll("section")];
+
+// let options = {
+//   rootMargin: "0px",
+//   threshold: 0.25,
+// };
+
+// const callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     const { target } = entry;
+//     // console.log(entry, target);
+
+//     if (entry.intersectionRatio >= 0.25) {
+//       target.classList.add("is-visible");
+//     } else {
+//       target.classList.remove("is-visible");
+//     }
+//   });
+// };
+
+// const observer = new IntersectionObserver(callback, options);
+
+// sections.forEach((section, index) => {
+//   observer.observe(section);
+// });
+
+// window.onload(removeClass());
